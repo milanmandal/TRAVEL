ece2117779a4160255108769667caa87c8a7d9c3
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-07-06 01:25:58 +0530

    Updates

---
+++ precache-manifest.3b857188a6bd3ebe942c9828d5071e65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaf7cfd7f8b340023233c2dd7c49dac",
+    "url": "/TRAVEL/index.html"
+  },
+  {
+    "revision": "ad51c95fd575359cb2bf",
+    "url": "/TRAVEL/static/css/main.8d3cc272.chunk.css"
+  },
+  {
+    "revision": "6f971160c2c87ea50522",
+    "url": "/TRAVEL/static/js/2.c3c89791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TRAVEL/static/js/2.c3c89791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad51c95fd575359cb2bf",
+    "url": "/TRAVEL/static/js/main.b8cdf261.chunk.js"
+  },
+  {
+    "revision": "3f205d4758bfa897f641",
+    "url": "/TRAVEL/static/js/runtime-main.3ce25e6a.js"
+  },
+  {
+    "revision": "2edea47533a8e8d4ad93dc3e3b0e7fcd",
+    "url": "/TRAVEL/static/media/header-bg.jpg.2edea475.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/TRAVEL/static/media/map-image.f40ed5b1.png"
+  }
+]);
